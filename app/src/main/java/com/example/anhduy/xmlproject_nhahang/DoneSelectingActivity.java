9b28c3428009d065662fe5e797a978f601054598
quanhdm300997 <minhquan300997@gmail.com>
2018-10-30 18:08:46 +0700

    Test giao diện Order - chưa xong

--- app/src/main/java/com/example/anhduy/xmlproject_nhahang/DoneSelectingActivity.java
+++ app/src/main/java/com/example/anhduy/xmlproject_nhahang/DoneSelectingActivity.java
@@ -1,22 +1,46 @@
 package com.example.anhduy.xmlproject_nhahang;
-//Duy:alo test thấy gì ở đây ko
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.jar.Attributes;
 
 import de.codecrafters.tableview.TableView;
 
 public class DoneSelectingActivity extends Activity {
 
+
+    //đưa các món ăn đã chọn vào 1 listView
+    //Hiển thị các món ăn đó
+    //Xác nhận order bằng button
+
     TableView tableView;
-    TextView textview;
+    TextView labelTenMon;
+    TextView labelGia;
+    TextView labelMieuTa;
+    TextView textTenMon;
+    TextView textGia;
+    TextView textMieuTa;
+    ImageView imgView;
+
+    CustomAdapter customAdapter;
+    ListView listView;
 
     ArrayList<Menu> database;
     ArrayList<Integer> selectedFoodPosition;
@@ -26,33 +50,82 @@ public class DoneSelectingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doneselecting);
-
+        setContentView(R.layout.activity_confirmorder);
 
         Init();
 
-        String count = "";
-        for(int k = 0 ; k < selectedFoodPosition.size() ; k++){
-            if(k == 0){
-                count += database.get(selectedFoodPosition.get(k)).Name;
-            }else {
-                count = count + ";" + database.get(selectedFoodPosition.get(k)).Name;
-            }
-        }
-        textview.setText(count);
-
-
-
-
-
-
-
-
-        //Test chỉnh sữa
+        LoadListView();
+        //labelTenMon.setText(count);
     }
 
 
+    class CustomAdapter extends BaseAdapter {
 
+        @Override
+        public int getCount() {
+            int count = 0;
+
+            //Đưa các món ăn đã chọn lên listView
+
+
+
+            return count;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.activity_doneselecting, null);
+
+            String img = "";
+            String name = "";
+            String priceSmall = "";
+            String priceBig = "";
+
+            //Nếu không sử dụng listView thì cả phần này chỉ hiển thị 1 món đã chọn
+            //Nếu chọn 2 món -> bị đè
+            for(i = 0 ; i < selectedFoodPosition.size() ; i++){
+                if(i == 0){
+                    img += database.get(selectedFoodPosition.get(i)).ImageUrl;
+                    name += database.get(selectedFoodPosition.get(i)).Name;
+                    priceSmall += database.get(selectedFoodPosition.get(i)).PriceSmall;
+                    priceBig += database.get(selectedFoodPosition.get(i)).PriceBig;
+
+                }else {
+                    img = img + ";" + database.get(selectedFoodPosition.get(i)).ImageUrl;
+                    name = name + ";" + database.get(selectedFoodPosition.get(i)).Name;
+                    priceSmall = priceSmall + ";" + database.get(selectedFoodPosition.get(i)).PriceSmall;
+                    priceBig = priceBig + ";" + database.get(selectedFoodPosition.get(i)).PriceBig;
+
+                }
+
+                textTenMon.setText(name);
+                textGia.setText("N: " + priceSmall);
+                textMieuTa.setText("L: " + priceBig);
+            }
+            return view;
+        }
+    }
+
+    private void LoadListView(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                listView.setAdapter(customAdapter);
+            }
+        }, 2000);   //chờ 2 second cho database load xong rồi mới hiện wiew
+        //response = task.response;
+
+    }
 
     protected void Init(){
         callerIntent = getIntent();
@@ -62,7 +135,19 @@ public class DoneSelectingActivity extends Activity {
         database = MainActivity.database;
         selectedFoodPosition = packageFromCaller.getIntegerArrayList("SelectedFood");
 
-        textview = (TextView)findViewById(R.id.textview);
+        customAdapter = new CustomAdapter();
+        listView = findViewById(R.id.listView);
+
+        labelTenMon = findViewById(R.id.labelTen);
+        labelGia = findViewById(R.id.labelGia);
+        labelMieuTa = findViewById(R.id.labelMieuTa);
+        imgView = findViewById(R.id.foodImage);
+
+        textTenMon = findViewById(R.id.text_TenMon);
+        textGia = findViewById(R.id.text_Gia);
+        textMieuTa = findViewById(R.id.text_MieuTa);
+
+        //textview = (TextView)findViewById(R.id.textview);
         //tableView = (TableView)findViewById(R.id.tableView);
     }
 }
